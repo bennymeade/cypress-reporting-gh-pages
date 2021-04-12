51cc369ead42b4c71ef2ac2449717f4852d3d1e4
bennymeade <brendan_meade@yahoo.co.uk>
2021-04-12 22:48:03 +0200

    updated test:after:run

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -28,21 +28,21 @@ import addContext from 'mochawesome/addContext'
 
 // const titleToFileName = (title) => title.replace(/[:\/]/g, '');
 
-// Cypress.on('test:after:run', (test, runnable) => {
-//     if (test.state === 'failed') {
-//         const filename = `${titleToFileName(runnable.parent.title)}-${titleToFileName(test.title)}-failed.png`;
-//         addContext({ test }, `../cypress/screenshots/${Cypress.spec.name}/${filename}`);
-//         addContext({ test }, `../cypress/videos/${Cypress.spec.name}.mp4`);
-//         // addContext({ test }, `../screenshots/${Cypress.spec.name}/${filename}`);
-//         // addContext({ test }, `../videos/${Cypress.spec.name}.mp4`);
-//     }
-// });
-
-Cypress.on("test:after:run", (test, runnable) => {
-    
-    let videoName = Cypress.spec.name
-    videoName = videoName.replace('/.js.*', '.js')
-    const videoUrl = 'videos/' + videoName + '.mp4'
-
-    addContext({ test }, videoUrl)
+Cypress.on('test:after:run', (test, runnable) => {
+    if (test.state === 'failed') {
+        const filename = `${titleToFileName(runnable.parent.title)}-${titleToFileName(test.title)}-failed.png`;
+        // addContext({ test }, `../cypress/screenshots/${Cypress.spec.name}/${filename}`);
+        // addContext({ test }, `../cypress/videos/${Cypress.spec.name}.mp4`);
+        addContext({ test }, `screenshots/${Cypress.spec.name}/${filename}`);
+        addContext({ test }, `videos/${Cypress.spec.name}.mp4`);
+    }
 });
+
+// Cypress.on("test:after:run", (test, runnable) => {
+    
+//     let videoName = Cypress.spec.name
+//     videoName = videoName.replace('/.js.*', '.js')
+//     const videoUrl = 'videos/' + videoName + '.mp4'
+
+//     addContext({ test }, videoUrl)
+// });
